07f69fa3d5a5312869eb4f44a09b3217d02f0360
qarakeseknurbol <kuanishulin@gmail.com>
2026-04-27 20:46:32 +0500

    Update football_game.py

--- football_game.py
+++ football_game.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="Head Soccer: Messi vs Ronaldo", layout="centered")
-st.title("⚽️ Super Cup: Almaty Edition")
+st.set_page_config(page_title="Head Soccer GOAT", layout="centered")
+st.title("⚽️ Super Cup: Messi vs Ronaldo")
 
-# Игровой движок
 game_code = """
 <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
 <script>
@@ -12,42 +11,40 @@ let p1, p2, ball;
 let gravity = 0.6;
 let round = 1, timer = 90, score1 = 0, score2 = 0;
 let isGameOver = false;
-
 let messiImg, ronaldoImg, ballImg, bgImg, goalImg;
 
 function preload() {
-  // Используем прямые ссылки на сырые файлы из GitHub (Raw)
-  // ВАЖНО: Замени 'qarakaseknurbol' на свое имя пользователя, если оно другое
-  let baseUrl = 'https://raw.githubusercontent.com/qarakaseknurbol/marketing/main/';
-  
-  messiImg = loadImage(baseUrl + 'messi.png');
-  ronaldoImg = loadImage(baseUrl + 'ronaldo.png');
-  ballImg = loadImage(baseUrl + 'ball.png');
-  bgImg = loadImage(baseUrl + 'background.png');
-  goalImg = loadImage(baseUrl + 'goal.png');
+  // Используем прямой путь. Если картинка не найдется, P5 просто выдаст ошибку в консоль, но НЕ зависнет
+  let url = 'https://raw.githubusercontent.com/qarakaseknurbol/marketing/main/';
+  messiImg = loadImage(url + 'messi.png', () => {}, () => { console.log('No Messi'); });
+  ronaldoImg = loadImage(url + 'ronaldo.png', () => {}, () => { console.log('No Ronaldo'); });
+  ballImg = loadImage(url + 'ball.png', () => {}, () => { console.log('No Ball'); });
+  bgImg = loadImage(url + 'background.png', () => {}, () => { console.log('No BG'); });
+  goalImg = loadImage(url + 'goal.png', () => {}, () => { console.log('No Goal'); });
 }
 
 function setup() {
-  let canvas = createCanvas(800, 500);
+  createCanvas(800, 500);
   p1 = new Player(100, messiImg, 65, 68, 87); 
   p2 = new Player(width - 200, ronaldoImg, LEFT_ARROW, RIGHT_ARROW, UP_ARROW);
   ball = new Ball();
 }
 
 function draw() {
+  // Фон: если картинка не загрузилась, будет небо
   if (bgImg && bgImg.width > 1) { background(bgImg); } 
   else { background(135, 206, 235); }
   
-  fill(34, 139, 34, 150);
-  rect(0, height - 30, width, 30);
+  // Трава
+  fill(34, 139, 34); rect(0, height - 30, width, 30);
   
+  // Ворота
   if (goalImg && goalImg.width > 1) {
      image(goalImg, 0, height - 200, 80, 180);
-     push();
-     translate(width, height - 200);
-     scale(-1,1);
-     image(goalImg, 0, 0, 80, 180);
-     pop();
+     push(); translate(width, height - 200); scale(-1,1);
+     image(goalImg, 0, 0, 80, 180); pop();
+  } else {
+     fill(255, 255, 255, 100); rect(0, height-200, 20, 170); rect(width-20, height-200, 20, 170);
   }
 
   if (!isGameOver) {
@@ -71,31 +68,21 @@ function resetLevel() {
 }
 
 function showUI() {
-    fill(0, 0, 0, 120);
-    rect(0, 0, width, 60);
+    fill(0, 0, 0, 120); rect(0, 0, width, 60);
     fill(255); textAlign(CENTER); textSize(32);
     text(score1 + " : " + score2, width/2, 40);
     textSize(20); textAlign(LEFT); text("Round: " + round, 20, 35);
     textAlign(RIGHT); text("Time: " + timer + "s", width - 20, 35);
-    if (isGameOver) {
-        fill(0, 150); rect(0,0, width, height);
-        fill(255); textAlign(CENTER); textSize(60);
-        let winner = score1 > score2 ? "Messi Wins!" : (score1 < score2 ? "Ronaldo Wins!" : "Draw!");
-        text("GAME OVER", width/2, height/2 - 20);
-        textSize(40); text(winner, width/2, height/2 + 40);
-    }
 }
 
 class Player {
   constructor(x, img, l, r, u) {
-    this.x = x; this.y = height - 130;
-    this.w = 90; this.h = 110;
-    this.img = img; this.vy = 0;
-    this.l = l; this.r = r; this.u = u;
+    this.x = x; this.y = height - 130; this.w = 90; this.h = 110;
+    this.img = img; this.vy = 0; this.l = l; this.r = r; this.u = u;
   }
   show() {
     if (this.img && this.img.width > 1) { image(this.img, this.x, this.y, this.w, this.h); } 
-    else { fill(200); rect(this.x, this.y, this.w, this.h); }
+    else { fill(this.l == 65 ? 'red' : 'blue'); rect(this.x, this.y, 60, 60); }
   }
   update() {
     if (keyIsDown(this.l)) this.x -= 8;
@@ -123,12 +110,10 @@ class Ball {
   }
   checkCol(p) {
     if (this.x > p.x && this.x < p.x + p.w && this.y > p.y && this.y < p.y + p.h) {
-      this.vy = -10;
-      this.vx = (this.x - (p.x + p.w/2)) * 0.6;
+      this.vy = -10; this.vx = (this.x - (p.x + p.w/2)) * 0.6;
     }
   }
 }
 </script>
 """
-
 components.html(game_code, height=550, width=850)
